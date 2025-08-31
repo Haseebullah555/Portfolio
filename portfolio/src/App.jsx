0f9a37673f51e0876a617b1fd9164db1f3d78d77
Hasibullah Shirzad <hasibullahshirzadahsf@gmail.com>
2025-08-31 07:30:40 +0430

    add about me section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import NavbarMain from './components/navbar/NavbarMain'
 import HeroMain from './components/heroSection/HeroMain'
+import SubHeroSection from './components/heroSection/SubHeroSection'
+import AboutMeMain from './components/AboutMe/AboutMeMain'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return <main className='font-body'>
     <NavbarMain/>
     <HeroMain/>
+    <SubHeroSection/>
+    <AboutMeMain/>
   </main>
 }
 
--- portfolio/src/components/heroSection/SubHeroSection.jsx
+++ portfolio/src/components/heroSection/SubHeroSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SubHeroSection = () => {
   return (
-    <div>SubHeroSection</div>
+    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:4xl md:text-2xl sm:4xl py-8 items-center gap-4'>
+      <p className='md:block sm:hidden'>Fast Learner</p>
+      <p className='md:block sm:hidden'>Team Work</p>
+      <p>Details Master</p>
+    </div>
   )
 }
 
